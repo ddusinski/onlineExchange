0eb687922bfa97fc7a48da9d1e800f888e33f88b
Dominik <dominik.dusinski@gmail.com>
2018-05-26 15:29:29 +0200

    new file:   .idea/vcs.xml 	modified:   .idea/workspace.xml 	modified:   src/main/java/onlineExchange/GoldPriceList.java 	modified:   src/main/java/onlineExchange/WebController.java 	modified:   src/main/resources/templates/goldGraph.html 	modified:   target/classes/onlineExchange/GoldPriceList.class 	modified:   target/classes/onlineExchange/WebController.class

--- src/main/java/onlineExchange/GoldPriceList.java
+++ src/main/java/onlineExchange/GoldPriceList.java
@@ -1,7 +1,16 @@
 package onlineExchange;
 
+import org.springframework.web.client.RestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class GoldPriceList {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public GoldPrice[] item;
+    public int itemcounter;
 
     public GoldPrice[] getItem() {
         return item;
@@ -10,8 +19,53 @@ public class GoldPriceList {
     public void setItem(GoldPrice[] item) {
         this.item = item;
     }
-    public GoldPriceList(GoldPrice[] item)
+    public GoldPriceList(int itemcounter)
     {
-        this.item=item;
+        this.item=getPriceList(itemcounter);
+        this.itemcounter=itemcounter;
+        reasultPrint();
+    }
+
+    public int getItemcounter() {
+        return itemcounter;
+    }
+
+    public void setItemcounter(int itemcounter) {
+        this.itemcounter = itemcounter;
+    }
+
+
+    private GoldPrice[]  getPriceList(int counter)
+    {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject("http://api.nbp.pl/api/cenyzlota/last/" + counter + "/?format=json",GoldPrice[].class);
+    }
+    private void reasultPrint()
+    {
+        for (int i=0; i<item.length;i++)
+        {
+            log.info(item[i].toString());
+        }
+    }
+
+    public double[] getDataArray()
+    {
+        double[] dataArray = new double[this.itemcounter];
+        for (int i=0;i<this.itemcounter; i++)
+        {
+            dataArray[i]=item[i].getCena();
+        }
+        return dataArray;
+    }
+
+    public String[] getNameArray()
+    {
+        String[] nameArray = new String[this.itemcounter];
+
+        for (int i=0;i<this.itemcounter; i++)
+        {
+            nameArray[i]=item[i].getDate();
+        }
+        return nameArray;
     }
 }
--- src/main/java/onlineExchange/WebController.java
+++ src/main/java/onlineExchange/WebController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.ui.Model;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,31 +14,17 @@ import java.io.IOException;
 @Controller
 public class WebController {
 
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
 
     @GetMapping("/")
     public String showForm(Model model) {
 
-        //int[] datasarray= {1, 2, 3, 4, 5};
 
-// String[] namesarray = {"Red", "Blue", "Yellow", "Green", "Purple", };
-        GoldPriceList gpd = new GoldPriceList(queryToClass());
-
-        double[] datasarray = new double[5];
-        String[] namesarray = new String[5];
-
-        for (int i=0;i<5; i++)
-        {
-            datasarray[i]=gpd.item[i].getCena();
-            namesarray[i]=gpd.item[i].getDate();
-        }
-
-
-
+        GoldPriceList gpd = new GoldPriceList( 10);
 
         model.addAttribute("GoldPriceList", gpd);
-        model.addAttribute("datasarray", datasarray);
-        model.addAttribute("namesarray", namesarray);
+        model.addAttribute("datasarray", gpd.getDataArray());
+        model.addAttribute("namesarray", gpd.getNameArray());
         return "goldGraph";
 
     }
@@ -50,18 +33,7 @@ public class WebController {
 
 
 
-    private static GoldPrice[] queryToClass()
-    {
-        RestTemplate restTemplate = new RestTemplate();
 
-        GoldPrice[] cenaZlota = restTemplate.getForObject("http://api.nbp.pl/api/cenyzlota/last/5/?format=json",GoldPrice[].class);
-        for (int i=0; i<cenaZlota.length;i++)
-        {
-            log.info(cenaZlota[i].toString());
-        }
-    return cenaZlota;
-
-    }
 
 
 
